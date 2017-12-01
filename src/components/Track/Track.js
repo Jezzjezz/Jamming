f0f6105d0a21de3cbf343d3d8e71d95df728bbd7
Jezzjezz <32496635+Jezzjezz@users.noreply.github.com>
2017-11-30 19:08:43 -0600

    Update Track.js

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -19,7 +19,7 @@ renderAction() {
   if (this.props.isRemoval){
   return <a className= "Track-action" onClick={this.removeTrack}>-</a>;
 } else {
-    return < a className="Track-action" onClick={this.addTrack}>+</a>;
+    return <a className="Track-action" onClick={this.addTrack}>+</a>;
     }
   }
 
@@ -28,9 +28,8 @@ renderAction() {
     <div className="Track" >
       <div className="Track-information">
         <h3>{this.props.track.name}</h3>
-        <p>{this.props.track.artist}| {this.props.track.album}</p>
+        <p>{this.props.track.artist} | {this.props.track.album}</p>
       </div>
-    //  <a className="Track-action"/>
       {this.renderAction()}
   </div>
   );
