a2ba17a6bacc2f10b78fef80db19046441de1796
Jezzjezz <32496635+Jezzjezz@users.noreply.github.com>
2017-11-30 19:07:58 -0600

    Update SearchResults.js

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -6,10 +6,10 @@ import './SearchResults.css';
    render() {
      return (
        <div className="SearchResults">
-  <h2>Results</h2>
-  <TrackList tracks={this.props.searchResults}  onAdd={this.props.onAdd} isRemoval={false}/>
-</div>
-     )
+        <h2>Results</h2>
+        <TrackList tracks={this.props.searchResults}  onAdd={this.props.onAdd} />
+      </div>
+    );
    }
  }
 
