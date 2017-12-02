9f25acfca5dc9ca1640b3e011f4c8174afdb13d8
Jezzjezz <32496635+Jezzjezz@users.noreply.github.com>
2017-12-01 19:45:54 -0600

    Update Playlist.js

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -15,7 +15,7 @@ constructor(props){
   render(){
     return (
       <div className="Playlist">
-      <input defaultvalue={'New Playlist'} onChange={this.handleNameChange}/>
+      <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
       <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove}/>
     <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
     </div>
